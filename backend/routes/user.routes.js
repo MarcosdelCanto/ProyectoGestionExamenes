d693877036a1fe1905ccbb8bc6530b2fcb56aff6
Isabel Veronica Calles Rojas <is.calles@duocuc.cl>
2025-05-02 13:09:14 -0400

    .

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 // routes/user.routes.js
 import express from 'express';
-import authMiddleware from '../middlewares/auth.middleware.js';
+import { authMiddleware } from '../middlewares/auth.middleware.js';
 import { getProfile } from '../controllers/user.controller.js';
 
 const router = express.Router();
